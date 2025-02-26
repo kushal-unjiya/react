2773d2976021ca9d3e5887d7d5abe6d855ac948c
Kushal Unjiya <130240616+kushal-unjiya@users.noreply.github.com>
2025-02-26 18:50:47 +0530

    implement counter functionality with add and remove buttons

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  let [counter,setCounter] = useState(15)
+
+  // let counter = 15
+  const removeValue = () => {
+    console.log('clicked', counter);
+    if (counter > 0) {
+      counter = counter - 1;
+      setCounter(counter);
+    }
+  }
+
+  const addValue = () => {
+    console.log('clicked', counter);
+    if (counter < 20) {
+      counter = counter + 1;
+      setCounter(counter);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>counter app</h1>
+      <h2>counter value: {counter} </h2>
+
+      <button onClick={addValue}>add value</button>
+      
+      <br />
+      <br />
+      
+      <button onClick={removeValue}>remove value</button>
     </>
   )
-}
-
+} 
 export default App
